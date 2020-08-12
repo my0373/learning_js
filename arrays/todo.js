d32f048c5a63fe653c3abfd381c26ce3edac66d3
Matt York <my0373@gmail.com>
2020-08-12 11:14:18 +0100

    First lesson and code test

---
+++ arrays/todo.js
@@ -0,0 +1,17 @@
+// Create an array with 5 things todo
+const todo = ['Go to Dentist',
+              'Pick up the kids',
+              'Finish my work',
+              'Clean the car',
+              'Join a call'];
+
+// print the length of the array inline.
+console.log(`You have ${todo.length} things to do.`);
+
+// print the first item explicitly
+console.log(`The first item is ${todo[0]}`)
+
+// print the second to last item by doing some inline loveliness.
+console.log(`The penultimate item is ${todo[todo.length -2]}`)
+
+
