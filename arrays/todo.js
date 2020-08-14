f4155f7c3be61a2f3280e59e9fcc3c703bedbb1e
Matt York <my0373@gmail.com>
2020-08-14 11:30:06 +0100

    Created a search method for objects.

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,26 +1,32 @@
-// Create an array with 5 things todo
-const todo = ['Go to Dentist',
-              'Pick up the kids',
-              'Finish my work',
-              'Clean the car',
-              'Join a call'];
+// Create an array of objects
+const taskobjects = [{
+    title: 'make coffee',
+    body: 'Remember to make a coffee'
+}, {
+    title: 'feed the guinea pigs',
+    body: 'Remember to feed blondie and fluffy'
+}]
 
-// Delete the 3rd item
-todo.splice(2,1)
-// Add a new item onto the end
-todo.push('New Item')
-// remove the first item from the list.
-todo.shift()
+// For arrays of objects, we use a callback function to test if the title matches the desired string.
+// If it does, we return the index of the matching object and print that element out.
 
-console.log(todo)
-// print each item in an array using foreach and a callback function
-// passing 2 variables. Item (the array item), and the index (the ordinal position).
-todo.forEach( function (item, index) {
-    console.log(` Item ${index + 1}: ${item} `)
-})
+// Define a function to look at the task objects and search for a supplied task title.
+const findTask = function (taskobjects, taskTitle) {
+    // Use the array findindex method to find an object whose title property
+    // matches the supplied taskTitle parameter. This will return the index of the matching object,
+    // or -1 if there is no match
+    const index = taskobjects.findIndex( function (task) {
+        return task.title === taskTitle;
+    })
+    // Attempt to retrieve the object at the specified index in the array
+    // if it is -1 (no match found) then there will be no array object and
+    // we will return undefined (as we have nothing to return)
+    return taskobjects[index];
 
-// a for loop!
-// Initial value, conditional, operation on iteration.
-for (let count = 1; count <= 10; count += 2 ) {
-    console.log(`Loop ${count}`);
 }
+
+// Call the search function and return the task
+task = findTask(taskobjects,'feed the guinea pigs')
+
+// print the task
+console.log(task)
