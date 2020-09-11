ad92eb1f36f074c4e18911f94dda0b894eb86781
Matt York <my0373@gmail.com>
2020-09-11 21:35:53 +0100

    Second excerise using filtering of lists and appending elements to the bottom of others.

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,11 +1,61 @@
-// remove all paragraph selectors that contain the word the.
+// The tasks are now an array in code.
+const tasks = [{
+    text: 'make coffee',
+    completed: true
+},{
+    text: 'feed the guinea pigs',
+    completed: true
+},{
+    text: 'Hide the bodies',
+    completed: true
+},{
+    text: 'buy acid',
+    completed: true
+},{
+    text: 'buy plastic buckets',
+    completed: false
+},{
+    text: 'Wash up',
+    completed: false
+},]
 
-const ps = document.querySelectorAll('p')
+// A couple of simple functions to allow us to pick out the tasks we want.
+function isComplete(task) {
+    return task.completed === true
+}
 
-ps.forEach( function (p ) {
-    console.log(p)
-    if (p.textContent.toLowerCase().includes('the')) {
-        console.log(`Removing element with text ${p.textContent}`)
-        p.remove()
+function isIncomplete(task) {
+    return task.completed === false
+}
+
+// We now run these filters on the array twice, counting the length of the array.
+let completed = tasks.filter(isComplete).length
+let uncompleted = tasks.filter(isIncomplete).length
+
+// Define the summary message
+const summaryText = `You have ${completed + uncompleted} tasks in total. ${completed} tasks have been completed. ${uncompleted} tasks are incomplete`
+
+// Go through each task and print it in green if completed, otherwise in red.
+// I could have done this using the same list filtering as above, but I wanted to show both methods.
+// Then append the task to the bottom of the body object
+tasks.forEach( function (task ) {
+    if (task.completed == true) {
+        const p = document.createElement('p')
+        p.textContent = task.text
+        p.style.color = "green"
+        document.querySelector('body').appendChild(p)
+    } else {
+        const p = document.createElement('p')
+        p.textContent = task.text
+        p.style.color = "red"
+        document.querySelector('body').appendChild(p)
     }
 })
+
+// create the summary and append it to the bottom of the body object
+const summary = document.createElement('p')
+summary.textContent = summaryText
+document.querySelector('body').appendChild(summary)
+
+// 1. print a summary message saying how many todos to complete
+// 2. Print a paragraph for each todo above. Us ethe text value as the label
