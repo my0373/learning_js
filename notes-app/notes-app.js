39fba9cbc9985b3df835da63f8b4d5ecc8ea011d
Matt York <my0373@gmail.com>
2020-09-14 09:40:52 +0100

    Playing around with dynamically modifying the body and colouring tasks based on their completeness.

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,10 +1,10 @@
 
-/*
-const p =  document.querySelector('p')
-
-console.log(p)
-p.remove()
- */
+const notes = [{
+    title: 'Arachnophobia',
+    body: 'I love spiders'},{
+    title: 'Postman pat',
+    body: 'And his black and white cat'
+}]
 
 const ps = document.querySelectorAll('p')
 console.log(ps)
@@ -12,3 +12,8 @@ ps.forEach(function (p) {
     console.log(p.textContent)
     p.textContent = "Hello Dave!"
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = "Hello Spider"
+document.querySelector('body').appendChild(newParagraph)
