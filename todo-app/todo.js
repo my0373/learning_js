88bb08318a68473678274ba42b822d6c1e017416
Matt York <my0373@gmail.com>
2020-09-15 09:58:36 +0100

    Created a proper form to add new items to the todo list

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -39,7 +39,6 @@ const renderTodos = function(todos, filters) {
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    console.log(filteredTodos)
     // For each
     filteredTodos.forEach( function (todo) {
 
@@ -51,6 +50,14 @@ const renderTodos = function(todos, filters) {
 
 }
 
+const addNewTodo = function(todos,newItem) {
+    newTodo = {
+        text: newItem,
+        completed: false
+    }
+    todos.push(newTodo)
+}
+
 // The entry point where we load all the todos.
 renderTodos(todos,filters)
 
@@ -58,11 +65,26 @@ renderTodos(todos,filters)
     ** Listeners **
 */
 
+
+// Set an event handler on the "add new todo form".
+// Override the default page refresh on submit and manually get the element based
+// on it's form name within the "target.elements" array.
+// We then add the new todo (on the assumption it's not complete)
+//  to the list and rerender the page.
+
+document.querySelector('form#new-todo-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    const newTodoName = e.target.elements.newTodoName.value
+    addNewTodo(todos,newTodoName)
+    renderTodos(todos,filters)
+    }
+)
+
 // This listener picks up changes to the text field. It updates the filters and rerenders the div.
 document.querySelector('input#filter-todos').addEventListener('input', function (e) {
-
 
     // Set the filter to the value of the search box.
     filters.searchText = e.target.value
     renderTodos(todos,filters)
+
 })
